1eaef89d1768fe7fe82d031371954c08f1473e46
om-abhiwan <om.abhiwan@gmail.com>
2025-06-14 11:50:47 +0530

    Intitial setup

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const app = require("express")();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hey its running!");
+})
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Server is running on port ${process.env.PORT} `)
+})
